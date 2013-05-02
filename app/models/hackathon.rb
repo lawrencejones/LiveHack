0830f18105e6ff0f6a578c24f726b066d756dd7d
Lawrence Jones <lawrence.jones12@imperial.ac.uk>
2013-05-02 02:07:00 +0100

    Prevented duplicate users, displays attendance

--- app/models/hackathon.rb
+++ app/models/hackathon.rb
@@ -1,7 +1,7 @@
 class Hackathon < ActiveRecord::Base
   attr_accessible :eid, :name, :start, :end, :location, :description
 
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, :uniq => true
   has_many :teams
   has_many :schedule_items
   has_many :proposals, :through => :teams
